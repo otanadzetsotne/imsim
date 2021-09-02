4f271c9c67265574bd566d15198a0ed35f60b757
otanadzetsotne <otanadzetsotne@yahoo.com>
2021-09-02 22:08:26 +0400

    Рефакторинг конфигурационного файла

--- config.py
+++ config.py
@@ -1,72 +1,53 @@
 import os
-from typing import Optional
-
-
-def check_directories(dirs: dict):
-    """ Creating directories if they are not exist """
-
-    for dir_ in dirs.values():
-        if dir_ is dict:
-            check_directories(dir_)
-
-        if not os.path.exists(dir_) and not os.path.isfile(dir_):
-            os.mkdir(dir_)
-
-
-class Configs:
-    __configs = {
-        # Project Directories
-        'directories': {
-            'models': os.path.abspath('models') if os.path.exists('models') else os.path.abspath('models'),
-            # Test Directories
-            'test': os.path.abspath('../tests'),
-            'test_images': os.path.abspath('../tests/data/images.txt'),
-            'test_dataset': os.path.abspath('../tests/data/dataset'),
-            'test_predictions': os.path.abspath('../tests/data/predictions'),
-        },
-        # Project Models
-        'models': {
-            'vit': {
-                'name': 'B_32_imagenet1k',
-                'image_size': 160,
-                'pretrained': True,
-            },
-            'encoder': {
-                'name': 'encoder_deeper',
-            },
-            'yolov5': {
-                'name': 'yolov5s',
-            },
-        },
-    }
-
-    @classmethod
-    def check_directories(cls):
-        """ Creating directories if they are not exist """
-
-        for dir_ in cls.get('directories'):
-            if not os.path.exists(dir_) or not os.path.isdir(dir_):
-                os.mkdir(dir_)
-
-    @classmethod
-    def get(cls, config: str):
-        return cls.__get(cls.__configs, config.split('.'))
-
-    @classmethod
-    def __get(cls, configs: Optional[dict], search: list):
-        if len(search) == 0 or configs is None:
-            return configs
-
-        configs = configs.get(search.pop(0))
-
-        return cls.__get(configs, search)
-
-
-# check_directories(Configs.get('directories'))
 
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
 
 MODEL_DIR = os.path.join(APP_DIR, 'models')
 
+MODEL_VIT_DIR = os.path.join(MODEL_DIR, 'vit')
 MODEL_VIT_INPUT = 720
 MODEL_VIT_NAME = 'B_16'
+
+
+REQUIRED_DIRECTORIES = [
+    MODEL_DIR,
+    MODEL_VIT_DIR,
+]
+
+
+if __name__ == '__main__':
+    def directory_exists(path: str):
+        """
+        Check if directory exists
+        :param path: path to directory
+        :return: bool
+        """
+
+        if os.path.isfile(directory_current):
+            raise FileExistsError(directory_current)
+
+        return os.path.exists(path)
+
+    # Create required directories
+    directories_checked = []
+    for directory in REQUIRED_DIRECTORIES:
+        directory = os.path.normpath(directory)
+
+        if directory_exists(directory):
+            continue
+
+        # Get all paths
+        directories = directory.split(os.sep)
+        # Root always exists
+        directory_current = directories.pop(0)
+        # Check each directory from root
+        for directory_to_check in directories:
+            directory_current = f'{directory_current}{os.sep}{directory_to_check}'
+
+            if directory_current in directories_checked:
+                continue
+
+            if not directory_exists(directory_current):
+                os.mkdir(directory_current)
+
+            directories_checked.append(directory_current)

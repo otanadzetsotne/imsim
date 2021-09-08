9408c912b359a47986c423cc3b5b648c2f9da4a1
otanadzetsotne <otanadzetsotne@yahoo.com>
2021-09-09 03:15:45 +0400

    Predictions internal algorithms done

--- internal/model_vit_predictions.py
+++ internal/model_vit_predictions.py
@@ -1,11 +1,8 @@
 import pickle
 import fire
 import os
-from itertools import repeat
-import PIL
-from multiprocessing import Pool
 from PIL import Image
-from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 
 from src.vit import ModelViT
 from config import IMAGE_PIL_FORMAT
@@ -22,21 +19,17 @@ class Predictor:
         self.path_images = path_images
         self.path_predictions = path_predictions
 
-    def predict_one(
+    def predict(
+            self,
+            image,
+    ):
+        return self.model.predict(image)
+
+    def image(
             self,
             image_name: str,
     ):
-        prediction_path = f'{self.path_predictions}/{image_name}.pickle'
-
-        if not os.path.exists(prediction_path):
-            image = Image.open(f'{self.path_images}/{image_name}').convert(IMAGE_PIL_FORMAT)
-            # with Image.open(f'{self.path_images}/{image_name}') as image:
-            #     image = image.convert(IMAGE_PIL_FORMAT)
-            prediction = self.model.predict(image)
-
-            with open(prediction_path, 'wb') as f:
-                print(f'Predicted {image_name}')
-                pickle.dump(prediction, f)
+        return Image.open(f'{self.path_images}/{image_name}').convert(IMAGE_PIL_FORMAT)
 
     def predict_map(
             self
@@ -46,14 +39,37 @@ class Predictor:
         print('Got images list')
         print(f'Images quantity: {len(images)}')
 
+        batch_size = 250
         print('Start mapping')
+        for batch_i in range(len(images) // batch_size):
+            print()
+            print('New iteration')
 
-        with ProcessPoolExecutor(1) as executor:
-            list(executor.map(self.predict_one, images))
+            batch_names = images[batch_i * batch_size:batch_i * batch_size + batch_size]
+            batch_names = [name for name in batch_names if not os.path.exists(f'{self.path_predictions}/{name}.pickle')]
+
+            if len(batch_names) == 0:
+                continue
+
+            with ProcessPoolExecutor() as executor:
+                batch_images = executor.map(self.image, batch_names)
+            print('Images collected')
+
+            batch_predictions = map(self.predict, batch_images)
+            print('Images predicted')
+
+            for i, prediction in enumerate(batch_predictions):
+                with open(f'{self.path_predictions}/{batch_names[i]}.pickle', 'wb') as f:
+                    pickle.dump(prediction, f)
+                    print('Predictions saved')
+
+
+def predict(
+        path_images: str,
+        path_predictions: str,
+):
+    Predictor(path_images, path_predictions).predict_map()
 
 
 if __name__ == '__main__':
-    p_images = 'C:\\Users\\otana\\Разработка\\py\\imsim_dataset\\google'
-    p_predictions = 'C:\\Users\\otana\\Разработка\\py\\imsim_predictions'
-
-    Predictor(p_images, p_predictions).predict_map()
+    fire.Fire()
---
+++ internal/model_vit_predictions_concat.py
@@ -0,0 +1,35 @@
+import os
+import pickle
+
+import fire
+import numpy as np
+from collections import deque
+
+
+def concat(
+        path_predictions: str,
+        path_predictions_concatenated: str,
+):
+    predictions = os.listdir(path_predictions)
+    predictions_quantity = len(predictions)
+    predictions = map(lambda x: f'{path_predictions}/{x}', predictions)
+    print('Got predictions')
+
+    predictions_concat = deque()
+    for prediction in predictions:
+        with open(prediction, 'rb') as f:
+            predictions_concat.append(pickle.load(f))
+    print('Got predictions list')
+
+    predictions_concat = np.array(predictions_concat)
+    print('Created predictions np.array')
+    predictions_concat = predictions_concat.reshape((predictions_quantity, -1))
+    print(f'Reshaped predictions np.array {predictions_concat.shape}')
+
+    with open(path_predictions_concatenated, 'wb') as f:
+        pickle.dump(predictions_concat, f)
+    print(f'Saved predictions np.array ({path_predictions_concatenated})')
+
+
+if __name__ == '__main__':
+    fire.Fire()
